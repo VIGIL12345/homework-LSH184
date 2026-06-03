0d8be3732ab8dbac945468a5ae72686741fcd52a
VIGIL12345 <3296935026@qq.com>
2026-06-03 20:42:33 +0800

    版本2：增加键盘输入输出功能

--- ex01/homework1.cpp
+++ ex01/homework1.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
+    char name[50];
+    
     printf("Hello World!\n");
+    printf("请输入你的名字：");
+    scanf("%s", name);
+    printf("你好，%s！欢迎学习C语言和GitHub\n", name);
+    
     return 0;
 }
